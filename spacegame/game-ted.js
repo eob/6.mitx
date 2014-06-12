98b643fae08d1d7a4ecc27802b4589a17fa08f03
Ted Benson <eob@csail.mit.edu>
2014-06-12 15:06:34 -0400

    stuff

--- spacegame/game-ted.js
+++ spacegame/game-ted.js
@@ -5,11 +5,58 @@ console.log("Hello!");
 
 function moveElementRight($elem, amount) {
   var startpoint = $elem.position().left;
-  var endpoint = startpoint + amount;
-  $elem.animate({
-    'left': endpoint + 'px'
-  });
+  var endpoint = parseInt(startpoint + amount);
+  var newCss = {
+    'transition': '1s ease-in-out',
+    '-webkit-transform': 'translate(' + endpoint + 'px, 0px)'
+  };
+  console.log(newCss);
+  $elem.css(newCss);
 };
+
+function loadEnemies(enemyName) {
+
+  var url = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  var data = {
+    tags: enemyName,
+    tagmode: "any",
+    format: "json"
+  };
+  var promise = $.ajax({
+    dataType: "json",
+    url: url,
+    data: data
+  });
+  $('img.enemy').remove();
+  promise.done(function(data) {
+    for (var i = 0; i < data.items.length; i++) {
+      var imgLink = data.items[i].media.m;
+      var newImg = $('<img class="enemy" src="' + imgLink + '" />');
+      $('body').append(newImg);
+    }
+  });
+  promise.fail(function(reason) {
+    console.log(reason);
+  });
+
+
+
+
+//
+//
+//
+//     .done(function( data ) {
+//       $.each( data.items, function( i, item ) {
+//         $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+//         if ( i === 3 ) {
+//           return false;
+//         }
+//       });
+//     });
+// })();
+
+};
+
 
 $(function() {
   var spaceship = $('.ship');
@@ -35,13 +82,27 @@ $(function() {
   $("#controls").append(score);
   $("#controls").append(input);
 
+
+  input.on('keyup', function(evt) {
+    if (evt.keyCode == 13) {
+      var enemyName = input.val();
+      console.log("Your chosen enemy:", enemyName);
+      loadEnemies(enemyName);
+    }
+  });
+
+
+
+
+
+
   // Just one
   // spaceship.css('left', '150px');
 
   // With an object
   spaceship.animate({
-    'left': '0px',
-    'top': '10px',
+    'left': '30px',
+    'top': '30px',
     '-webkit-transform': 'rotate(34deg)'
   }, 1000);
 
